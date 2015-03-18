18a1d3438df4dbf633620c290bb7e82aac703ea4
MightySCollins <theatrepro11@gmail.com>
2015-03-18 19:48:44 +0000

    SQL Cleaning

--- pages/newplant.php
+++ pages/newplant.php
@@ -4,13 +4,22 @@ if (!$connect) die(mysql_error());
 mysql_select_db(DB_NAME);
 
 if (isset($_POST['save'])) {
-    $location = $_POST['location'];
-    $product = $_POST['product'];
+    $location = filter_var($_POST['location'], FILTER_SANITIZE_NUMBER_INT);
+    $product = filter_var($_POST['product'], FILTER_SANITIZE_STRING);
     $sown = $_POST['sown'];
-    $qty = $_POST['qty'];
-    $available = $_POST['available'];
+    $qty = filter_var($_POST['qty'], FILTER_SANITIZE_NUMBER_INT);
+    $available = filter_var($_POST['available'], FILTER_SANITIZE_STRING);
+    if(!empty($location)
+        && is_numeric($location)
+        && !empty($product)
+        && preg_match('/^[a-z\d]{2,64}$/i', $product)
+        && !empty($qty)
+        && is_numeric($qty)
+        && !empty($available)
+        && preg_match('/^[a-z\d]{2,64}$/i', $available))
     $results = mysql_query("INSERT INTO plants
                             VALUES ('$location', '$product', '$sown', '$qty', '$available')");
+    else $message = '<div class=\'alert alert-warning\'>Some inputs are invalid</div>';
 }
 ?>
 
@@ -27,7 +36,7 @@ if (isset($_POST['save'])) {
                                 <label for="plant_input_location" class="col-sm-2 control-label">Location</label></div>
                             <div class="col-md-4">
                                 <input class="form-control" id="plant_input_location"
-                                       name="location" type="number">
+                                       name="location" type="number" required>
                             </div>
                         </div>
                     </div>
@@ -37,7 +46,7 @@ if (isset($_POST['save'])) {
                                 <label for="plant_input_location" class="col-sm-2 control-label">Product</label></div>
                             <div class="col-md-4">
                                 <input class="form-control" id="plant_input_product"
-                                       name="product" type="text"">
+                                       name="product" type="text" required>
                             </div>
                         </div>
                     </div>
@@ -47,7 +56,7 @@ if (isset($_POST['save'])) {
                                 <label for="plant_input_sown" class="col-sm-2 control-label">Sown</label></div>
                             <div class="col-md-4">
                                 <input class="form-control" id="plant_input_sown"
-                                       name="sown" type="date"">
+                                       name="sown" type="date" required>
                             </div>
                         </div>
                     </div>
@@ -57,7 +66,7 @@ if (isset($_POST['save'])) {
                                 <label for="plant_input_qty" class="col-sm-2 control-label">QTY</label></div>
                             <div class="col-md-4">
                                 <input class="form-control" id="plant_input_qty"
-                                       name="qty" type="number"">
+                                       name="qty" type="number" required>
                             </div>
                         </div>
                     </div>
@@ -67,7 +76,7 @@ if (isset($_POST['save'])) {
                                 <label for="plant_input_available" class="col-sm-2 control-label">Available</label></div>
                             <div class="col-md-4">
                                 <input class="form-control" id="plant_input_available"
-                                       name="available" type="text"">
+                                       name="available" type="text" required>
                             </div>
                         </div>
                     </div>
